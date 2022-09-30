615cbe9ab692b2693bbcac4d51f982a39ed62cc1
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-30 14:16:13 +0200

    Add project files.

---
+++ Mutte uppgift 3.14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//Uppgift 3.14
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Console.WriteLine("har du gått ut gymnasiet? j/n ");
+        string svar = Console.ReadLine().ToLower();
+        Console.WriteLine("Hur gammal är du");
+        int ålder = int.Parse(Console.ReadLine());
+        if (svar == "j" && ålder <= 22 && ålder >= 19)
+        {
+            Console.WriteLine("vi vill gärna anställa dig  ");
+        }
+
+        else
+        {
+            Console.WriteLine("Vi letar tyvärr efter annan personal just nu. ");
+        }
+
+
+
+
+    }
+}
